84274eb42ded6f619ca91b101560ab82c59d91ef
Naeem Baghi <baghinaeem@gmail.com>
2021-04-24 17:24:32 +0430

    add method to useApi

--- src/api/useApi.ts
+++ src/api/useApi.ts
@@ -3,30 +3,26 @@ import { useEffect, useState } from 'react';
 
 type Options = {
   isReview?: boolean;
+  method?: 'get' | 'post' | 'put' | 'delete';
 };
 
 const useApi = <T>(url: string, options?: Options): { data: T | undefined } => {
-  const { isReview } = options ?? {};
+  const { isReview, method } = options ?? {};
   const [data, setData] = useState();
-  console.log(
-    process.env.REACT_APP_REVIEW_URL,
-    process.env.REACT_APP_DEFAULT_URL,
-  );
   useEffect(() => {
     axios({
-      method: 'GET',
-      // baseURL: isReview
-      //   ? process.env.REACT_APP_REVIEW_URL
-      //   : process.env.REACT_APP_DEFAULT_URL,
-      // url,
-      url: 'http://localhost:3001/product',
+      method: method ?? 'get',
+      baseURL: isReview
+        ? process.env.REACT_APP_REVIEW_URL
+        : process.env.REACT_APP_DEFAULT_URL,
+      url,
     }).then(
       (res) => {
         setData(res.data);
       },
-      (e) => {},
+      () => {},
     );
-  }, [isReview, url]);
+  }, [isReview, url, method]);
   return { data };
 };
 
